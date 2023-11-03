eb7f8cb8cb1942290053899fabbc52bcf582f077
Sebastiao Muller <sebastiaomuller@MacBook-Air-de-Sebastiao.local>
2023-11-03 01:58:59 +0000

    pdating branch

--- header/Parse.hpp
+++ header/Parse.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Parse.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcoimbra <pcoimbra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:19:54 by sde-mull          #+#    #+#             */
-/*   Updated: 2023/11/02 17:25:16 by pcoimbra         ###   ########.fr       */
+/*   Updated: 2023/11/03 01:57:51 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 #include "Lincludes.hpp"
 #include "Client.hpp"
+#include "Channel.hpp"
 
 class Client;
+class Channel;
 class Parse
 {
     private:
